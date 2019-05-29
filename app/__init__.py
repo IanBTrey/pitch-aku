9ec47f695ea73bdd1eb7fc85bdc17223257d8bc6
Ian Brian Trey <kibetkirui010@gmail.comgit config --global user.name IanBTrey>
2019-05-29 10:53:08 +0300

    created the create_up function and initialized my app

--- app/__init__.py
+++ app/__init__.py
@@ -6,3 +6,17 @@ from flask_login import LoginManager
 from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_mail import Mail
 from flask_simplemde import SimpleMDE
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+mail = Mail()
+photos = UploadSet('photos',IMAGES)
+simple = SimpleMDE()
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
+def create_app(config_name):
+
+    app = Flask(__name__)
