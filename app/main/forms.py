ed201a6a3f159087c23de5dabd74ffe6731feaed
Ian Brian Trey <kibetkirui010@gmail.comgit config --global user.name IanBTrey>
2019-05-29 12:41:41 +0300

    implemented the imports

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,3 +1,7 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField,SelectField,ValidationError
+from wtforms.validators import Required,Email,EqualTo
+from ..models import User,Pitch,Comment,Upvote,Downvote
 
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('who are you mate?',validators=[Required()])
@@ -7,7 +11,7 @@ class PitchForm(FlaskForm):
     Category_id=SelectField("Select Category :", choices=[('c','select'),('AI','Artifitial Intelegence'),('R','Robotics'),('D','Drones'),('IOT','IoT'),('D','Data Science')])
     pitch=TextAreaField("In about 200 words post your pitch",validators=[Required()])
     submit=SubmitField("Add your pitch mate")
-    
+
 class CommentForm(FlaskForm):
     comment=TextAreaField("post your comments",validators=[Required()])
     submit=SubmitField("Add your comments mate")
