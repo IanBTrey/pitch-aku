2291a957e13a13fef9627577a87314b0013c95a9
Ian Brian Trey <kibetkirui010@gmail.comgit config --global user.name IanBTrey>
2019-05-29 10:34:09 +0300

    created setup method

--- tests/test_user.py
+++ tests/test_user.py
@@ -4,3 +4,8 @@ class UserModelTest(unittest.TestCase):
     Args:
         unittest.TestCase : Test case class that helps create test cases
     '''
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_user = User(password = '5575')
