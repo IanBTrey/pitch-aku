e3c3b5119266807c243a36c7885527238eec196b
Ian Brian Trey <kibetkirui010@gmail.comgit config --global user.name IanBTrey>
2019-05-29 10:33:15 +0300

    created test class to test user class behaviours

---
+++ tests/test_user.py
@@ -0,0 +1,6 @@
+class UserModelTest(unittest.TestCase):
+    '''
+    Test class to test behaviours of the User class
+    Args:
+        unittest.TestCase : Test case class that helps create test cases
+    '''
