7716da8001670c85bc37b9d8d54b05c2dec02f84
Ian Brian Trey <kibetkirui010@gmail.comgit config --global user.name IanBTrey>
2019-05-29 11:51:06 +0300

    created the pitch category form

--- app/main/forms.py
+++ app/main/forms.py
@@ -2,3 +2,8 @@
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('who are you mate?',validators=[Required()])
     submit=SubmitField("Submit")
+
+class PitchForm(FlaskForm):
+    Category_id=SelectField("Select Category :", choices=[('c','select'),('AI','Artifitial Intelegence'),('R','Robotics'),('D','Drones'),('IOT','IoT'),('D','Data Science')])
+    pitch=TextAreaField("In about 200 words post your pitch",validators=[Required()])
+    submit=SubmitField("Add your pitch mate")
