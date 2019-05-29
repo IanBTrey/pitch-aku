bf747f6abbcf57e65386b27eeaea181e0dbdb393
Ian Brian Trey <kibetkirui010@gmail.comgit config --global user.name IanBTrey>
2019-05-29 10:54:03 +0300

    created the app configurations

--- app/__init__.py
+++ app/__init__.py
@@ -20,3 +20,12 @@ login_manager.login_view = 'auth.login'
 def create_app(config_name):
 
     app = Flask(__name__)
+
+    # Creating the app configurations
+
+    app.config.from_object(config_options[config_name])
+    # config_options[config_name].init_app(app)
+
+
+
+    # Configure UploadSet
