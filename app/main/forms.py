046f4c7fb66df53722ee71af436f215125f5e2a0
Ian Brian Trey <kibetkirui010@gmail.comgit config --global user.name IanBTrey>
2019-05-29 11:51:43 +0300

    created the comments form

--- app/main/forms.py
+++ app/main/forms.py
@@ -7,3 +7,7 @@ class PitchForm(FlaskForm):
     Category_id=SelectField("Select Category :", choices=[('c','select'),('AI','Artifitial Intelegence'),('R','Robotics'),('D','Drones'),('IOT','IoT'),('D','Data Science')])
     pitch=TextAreaField("In about 200 words post your pitch",validators=[Required()])
     submit=SubmitField("Add your pitch mate")
+    
+class CommentForm(FlaskForm):
+    comment=TextAreaField("post your comments",validators=[Required()])
+    submit=SubmitField("Add your comments mate")
